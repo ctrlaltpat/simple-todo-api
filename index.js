8672fcfd43bf60cb9e46c7805033afa9264af23f
Patrick <ctrlaltpat@gmail.com>
2019-01-15 17:21:28 +0000

    simple test routes

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = express()
 const port = 3005
 
 app.get('/', (req, res) => res.send('Super Simple!'))
+app.get('/patrick', (req, res) => res.send('Patrick is an amazing dev!'))
 
 app.listen(port, () => console.log(`Basic api is running on port ${port}`))
