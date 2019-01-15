4d040facbf0081a267b58ba6eb8c9ee1a1c56b2f
Patrick <ctrlaltpat@gmail.com>
2019-01-15 19:08:44 +0000

    fixed spelling

--- models/index.js
+++ models/index.js
@@ -2,6 +2,6 @@ let mongoose = require('mongoose')
 mongoose.set('debug', true) // debug mode 
 mongoose.connect('mongodb://localhost/simple-todo-api') // connect to db 
 
-mongoode.Promise = Promise // allows us to use promise syntax
+mongoose.Promise = Promise // allows us to use promise syntax
 
 module.exports.Todo = require("./todo")
