8e9abea62241dcede53d6bc453ea79e16541bc4f
Patrick <ctrlaltpat@gmail.com>
2019-01-15 18:29:18 +0000

    simple GETs

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 3005
 
 app.get('/', (req, res) => res.send('Super Simple!'))
-app.get('/patrick', (req, res) => res.send('Patrick is an amazing dev!'))
+app.get('/patrick', (req, res) => res.send('<h1>Patrick is an amazing dev!</h1>'))
+app.get('/data', (req, res) => res.send({message: "Want some JSON?"}))
+app.get('/explicit_data', (req, res) => res.json({message: "Have some JSON!"}))
 
 app.listen(port, () => console.log(`Basic api is running on port ${port}`))
