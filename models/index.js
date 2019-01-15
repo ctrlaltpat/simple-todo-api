49ef7ce8fd5bc841728f2132adac782f28380dbb
Patrick <ctrlaltpat@gmail.com>
2019-01-15 18:41:26 +0000

    basic mongoose and Todo model setup with Schema

---
+++ models/index.js
@@ -0,0 +1,7 @@
+let mongoose = require('mongoose')
+mongoose.set('debug', true) // debug mode 
+mongoose.connect('mongodb://localhost/simple-todo-api') // connect to db 
+
+mongoode.Promise = Promise // allows us to use promise syntax
+
+module.exports.Todo = require("./todo")
