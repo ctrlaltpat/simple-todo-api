1d0623e630adc81e0e21da2295f309fdc8413b66
Patrick <ctrlaltpat@gmail.com>
2019-01-15 19:10:45 +0000

    basic /api/todos setup

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3005
 
-app.get('/', (req, res) => res.send('Super Simple!'))
-app.get('/patrick', (req, res) => res.send('<h1>Patrick is an amazing dev!</h1>'))
-app.get('/data', (req, res) => res.send({message: "Want some JSON?"}))
-app.get('/explicit_data', (req, res) => res.json({message: "Have some JSON!"}))
+let todoRoutes = require('./routes/todos')
+
+app.get('/', (req, res) => res.send('Hello form the root route'))
+
+app.use('/api/todos', todoRoutes)
+
+
+
+// app.get('/', (req, res) => res.send('Super Simple!'))
+// app.get('/patrick', (req, res) => res.send('<h1>Patrick is an amazing dev!</h1>'))
+// app.get('/data', (req, res) => res.send({message: "Want some JSON?"}))
+// app.get('/explicit_data', (req, res) => res.json({message: "Have some JSON!"}))
 
 app.listen(port, () => console.log(`Basic api is running on port ${port}`))
